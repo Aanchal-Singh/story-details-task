64fd7beed7c162ead1935db64747b8b843d29b2d
Aanchal-Singh <aanchalsingh18@gmail.com>
2016-11-17 14:49:12 +0530

    first commit- repo set up

---
+++ app/src/main/java/com/example/aanchalsingh/roposo_stories_task/Activities/Stories.java
@@ -0,0 +1,15 @@
+package com.example.aanchalsingh.roposo_stories_task.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aanchalsingh.roposo_stories_task.R;
+
+public class Stories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stories);
+    }
+}
